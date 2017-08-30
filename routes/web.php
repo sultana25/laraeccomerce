732a80bb6dcdc616b478de0868716f5a21dc8050
sultana25 <ssazeda@gmail.com>
2017-08-29 21:13:34 -0700

    adding user login system

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/','WelcomeController@index');
 Route::get('/category','WelcomeController@category');
 Route::get('/product-details','WelcomeController@productDetails');
 Route::get('/contact','WelcomeController@contact');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
